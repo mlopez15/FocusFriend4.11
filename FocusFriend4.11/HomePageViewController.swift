b1f12ddbe52532309e269383dfb878295b0a48fb
Madeline Lopez <madeline@facstaffmac-3.local>
2017-04-11 12:40:19 -0400

    initialization of new project to resolve sql lite problems

---
+++ FocusFriend4.11/HomePageViewController.swift
@@ -0,0 +1,40 @@
+//
+//  HomePageViewController.swift
+//  FocusFriend4.11
+//
+//  Created by Madeline on 4/11/17.
+//  Copyright © 2017 Madeline. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    @IBOutlet weak var PointsTotal: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        PointsTotal.layer.cornerRadius = PointsTotal.frame.height/2
+        PointsTotal.layer.masksToBounds = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
